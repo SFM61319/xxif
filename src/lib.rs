c1cd25b6926647ea27b9c5b2c041dfc88adb4202
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2024-04-12 13:24:59 +0530

    chore(refactor): Refactor `xxif` lib crate

--- src/lib.rs
+++ src/lib.rs
@@ -2,84 +2,89 @@
 
 use std::ffi::OsStr;
 use std::fs::read;
+use std::io::Result as IoResult;
 use std::path::{Path, PathBuf};
 
 use little_exif::{exif_tag::ExifTag, metadata::Metadata};
 use rayon::prelude::*;
-use rayon::{ThreadPool, ThreadPoolBuilder};
+use rayon::ThreadPoolBuilder;
+use uuid::Uuid;
 use walkdir::{DirEntry, WalkDir};
-use xxhash_rust::xxh3::xxh3_64;
+use xxhash_rust::xxh3::xxh3_128;
 
 pub mod constants;
 
-/// Processes all images in `directory` in parallel using `cores` threads.
-pub fn process_images(directory: &str, cores: usize) {
-    let image_paths = walk_images(directory).map(DirEntry::into_path);
-    let pool = create_thread_pool(cores);
-
-    pool.install(|| image_paths.par_bridge().for_each(process_image));
-}
-
-/// Returns an iterator over all files in `directory` that are images.
+/// Returns `true` if `extension` is an acceptable image extension.
 #[inline]
-fn walk_images(directory: &str) -> impl Iterator<Item = DirEntry> {
-    WalkDir::new(directory)
-        .into_iter()
-        .filter_map(Result::ok)
-        .filter(is_file)
-        .filter(is_image)
-}
-
-/// Returns `true` if `entry` is a file.
-#[inline]
-fn is_file(entry: &DirEntry) -> bool {
-    entry.file_type().is_file()
+fn is_acceptable(extension: &str) -> bool {
+    constants::ACCEPTABLE_IMAGE_EXTENSIONS
+        .iter()
+        .any(|&acceptable_extension| extension.eq_ignore_ascii_case(acceptable_extension))
 }
 
 /// Returns `true` if `entry` is an image.
 #[inline]
 fn is_image(entry: &DirEntry) -> bool {
-    let image_extension = constants::IMAGE_EXTENSION;
     entry
         .path()
         .extension()
         .and_then(OsStr::to_str)
-        .map_or(false, |ext| ext.eq_ignore_ascii_case(image_extension))
+        .map_or(false, is_acceptable)
 }
 
-/// Creates a new thread pool with `cores` threads.
-#[inline]
+/// Hashes the image at `path` using `XXH3-128` and returns it.
+fn hash_image(path: &Path) -> IoResult<u128> {
+    let bytes = read(path)?;
+    let hash_value = xxh3_128(&bytes);
+
+    tracing::debug!(hash = ?hash_value, "Hashed image");
+    Ok(hash_value)
+}
+
+/// Processes the image at `path` and returns it.
 #[tracing::instrument(level = tracing::Level::DEBUG)]
-fn create_thread_pool(cores: usize) -> ThreadPool {
+fn process_image(path: PathBuf) -> IoResult<()> {
+    let hash_value = hash_image(&path)?;
+    let mut metadata = Metadata::new();
+
+    let image_uid = Uuid::from_u128(hash_value).hyphenated().to_string();
+    let image_uid = ExifTag::ImageUniqueID(image_uid);
+
+    metadata.set_tag(image_uid);
+    metadata.write_to_file(&path)?;
+
+    tracing::info!(image = ?path, "Processed image");
+    Ok(())
+}
+
+/// Processes all images in `directory` recursively.
+/// Returns the number of successfully processed images.
+#[inline]
+fn process_all_images<P>(directory: P) -> usize
+where
+    P: AsRef<Path>,
+{
+    WalkDir::new(directory)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|entry| entry.file_type().is_file() && is_image(entry))
+        .map(DirEntry::into_path)
+        .par_bridge()
+        .map(process_image)
+        .filter_map(Result::ok)
+        .count()
+}
+
+/// Processes all images in `directory` in parallel using `cores` threads.
+/// Returns the number of successfully processed images.
+#[inline]
+pub fn process_images<P>(directory: P, cores: usize) -> usize
+where
+    P: AsRef<Path> + Send,
+{
     ThreadPoolBuilder::new()
         .num_threads(cores)
         .build()
-        .expect("Failed to create thread pool")
-}
-
-/// Processes the image at `path`.
-#[tracing::instrument(level = tracing::Level::DEBUG)]
-fn process_image(path: PathBuf) {
-    tracing::info!("Processing image");
-
-    let hash_value = hash_image(&path);
-    let mut metadata = Metadata::new();
-
-    metadata.set_tag(ExifTag::ImageUniqueID(hash_value.to_string()));
-    metadata
-        .write_to_file(&path)
-        .expect("Failed to write image metadata");
-
-    tracing::info!("Processed image");
-}
-
-/// Hashes the image at `path`.
-fn hash_image(path: &Path) -> u64 {
-    tracing::debug!("Hashing image");
-
-    let bytes = read(path).expect("Failed to open and read image");
-    let hash_value = xxh3_64(&bytes);
-
-    tracing::debug!("Hashed image");
-    hash_value
+        .map(|pool| pool.install(|| process_all_images(directory)))
+        .unwrap_or_default()
 }
